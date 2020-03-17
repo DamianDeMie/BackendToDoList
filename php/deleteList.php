112515e1a8e35b85189430e6fbd54925d6b66a74
DamianDeMie <damiandemie@gmail.com>
2020-03-17 13:40:27 +0100

    updated delete List feature

--- php/deleteList.php
+++ php/deleteList.php
@@ -3,6 +3,9 @@ include 'connectToDB.php';
 
 $stmt = $conn->prepare("DELETE FROM lists WHERE list_id = :list_id");
 $stmt->bindParam(':list_id', $_GET['id'], PDO::PARAM_INT);
+$stmt2 = $conn->prepare("DELETE FROM tasks WHERE list_id = :list_id");
+$stmt2->bindParam(':list_id', $_GET['id'], PDO::PARAM_INT);
 
 $stmt->execute();
+$stmt2->execute();
 header("location:../index.php");
