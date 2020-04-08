7775be95e3337f8ff2ed0463e76480ae6f6af5f3
DamianDeMie <damiandemie@gmail.com>
2020-04-08 12:45:48 +0200

    fixed delete bug with tasks

--- php/deleteTask.php
+++ php/deleteTask.php
@@ -4,3 +4,8 @@ include 'connectToDB.php';
 $stmt = $conn->prepare("DELETE FROM tasks WHERE task_id = :task_id");
 $stmt->bindParam(':task_id', $_GET['id'], PDO::PARAM_INT);
 $stmt->execute();
+
+
+
+header('Location: ' . $_SERVER['HTTP_REFERER']);
+exit;
